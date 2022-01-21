87211214d25753282afc4c856d869e1bd94b842b
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2022-01-21 07:18:49 +0100

    Update graphql-oracle-nosql.js
    https://github.com/dario-vega/demo-graphql-nosql/issues/1
    Move the prepare statement to the run function
    WA: adding a shadow copy to updateBlogHelper

--- graphql_nosql/graphql-oracle-nosql.js
+++ graphql_nosql/graphql-oracle-nosql.js
@@ -68,11 +68,12 @@ async function updateBlogHelper(id, input) {
   // Because we are using GENERATED ALWAYS AS IDENTITY I am using UPDATE command
   // We can use the command putIfPresent in other cases - see updateBlogHelperWithoutSeq
   //  https://docs.oracle.com/en/database/other-databases/nosql-database/19.5/java-driver-table/inserting-identity-values-programmatically.html
-  console.log(preparedStmt)
+  const preparedStmt = Object.assign( globalPreparedStmt);
   preparedStmt.bindings = {
     $v_id: id,
     $v_blog : input.blog
   };
+  console.log(preparedStmt)
   res = await client.query(preparedStmt);
   let updBlog = {id : id , blog: input.blog};
   return updBlog;
@@ -157,11 +158,11 @@ async function ws() {
 
 // Do it
 let client;
-let preparedStmt;
+let globalPreparedStmt;
 async function run() {
   client = createClient();
   statement = `DECLARE $v_blog STRING; $v_id INTEGER; UPDATE ${TABLE_NAME} SET blog = $v_blog WHERE id=$v_id`;
-  preparedStmt = await client.prepare(statement);	
+  globalPreparedStmt = await client.prepare(statement);	
   await ws();
 }
 
